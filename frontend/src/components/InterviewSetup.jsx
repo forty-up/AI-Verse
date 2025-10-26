826e3a6a39bebc99cd52891271b7fee49d07a312
forty-up <devsinblr@gmail.com>
2025-10-26 10:03:02 +0530

    api calls

--- frontend/src/components/InterviewSetup.jsx
+++ frontend/src/components/InterviewSetup.jsx
@@ -34,7 +34,7 @@ const InterviewSetup = ({ onStart }) => {
     setError(null)
 
     try {
-      const response = await fetch('http://localhost:5000/api/interview/generate-questions', {
+      const response = await fetch(API_ENDPOINTS.interview.generate, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
